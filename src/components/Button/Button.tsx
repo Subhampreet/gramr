41a09202d316f9127a20f3c48085c21a412e4d1b
Subhampreet <subhampreet18@gmail.com>
2023-02-21 23:59:10 +0530

    Package Bundled using Rollup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps{
     label: string;
 }
 
-function Button(ButtonProps) {
+function Button(ButtonProps: { label: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) {
   return (
     <button>{ButtonProps.label}</button>
   )
